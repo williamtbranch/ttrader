a14835b849302e09854a62ca2322c237e6bf8573
williamtbranch <williamtbranch@gmail.com>
2021-07-30 07:59:10 -0500

    added express server

--- services/tradestation.service.js
+++ services/tradestation.service.js
@@ -5,7 +5,7 @@ const simUrl = 'https://sim-api.tradestation.com/v2'
 
 const TradestationService = {
     getSymbol(symbol) {
-        return axios.get(sumUrl + "symbol/" + symbol)
+        return axios.get(simUrl + "symbol/" + symbol)
     }
 }
 
